d19ef9a818ac70210b4a605db69938de32dbb3c1
sunnuii <sarahcha04@gmail.com>
2025-03-19 23:07:26 -0400

    No TMDB Auth

--- search/search.js
+++ search/search.js
@@ -2,7 +2,6 @@ const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NWQ2MWQ3NzNkMTMzYjI2YWRiM2MwY2U2ODQzNmE4YSIsIm5iZiI6MTc0MjQxMjk1Mi41MjQ5OTk5LCJzdWIiOiI2N2RiMWM5ODhhZjQ1MmYzMGZlOWQwMTMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.XeV45F5ItmZeiv7r4ga9--hjwIyO7fB2f8AUTe_RbVY'
     }
   };
 
