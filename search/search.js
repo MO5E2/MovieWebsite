8747574950af79fcb3af8ab0c61b92b3a98a720e
sunnuii <sarahcha04@gmail.com>
2025-03-21 12:20:02 -0400

    trending movies

---
+++ search/search.js
@@ -0,0 +1,32 @@
+/*defines method and provides api key within the fetch() function*/
+
+  /*paste api above*/
+
+  async function fetchMovies() {
+    try {
+      const response = await fetch(apiUrl, options);
+      const data = await response.json();
+  
+      data.results.forEach(media => {
+        const movieCard = createMovieCard(media);
+        moviesContainer.appendChild(movieCard);
+      });
+    } 
+    catch (error) {
+        console.error("Error fetching data:", error);
+    }
+  }
+  
+  function createMovieCard(media) {
+    const { title, name, backdrop_path, release_date } = media;
+  
+    const movieCard= document.createElement("div");
+    movieCard.classList.add("movie_item")
+  
+    movieCard.innerHTML = `
+      <img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" class="movie_img_rounded">
+      <div class = "title">${title || name}</div>
+      `;
+      return movieCard;
+  }
+  fetchMovies();
